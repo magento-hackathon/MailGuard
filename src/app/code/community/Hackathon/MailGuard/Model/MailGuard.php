4f77e42f47be5dcb359802974fbf78d2a71f83fa
David Manners <dmanners87@gmail.com>
2014-05-10 18:46:46 +0200

    Add code note for getDomainName

--- src/app/code/community/Hackathon/MailGuard/Model/MailGuard.php
+++ src/app/code/community/Hackathon/MailGuard/Model/MailGuard.php
@@ -101,6 +101,7 @@ class Hackathon_MailGuard_Model_MailGuard extends Mage_Core_Model_Abstract
 
     /**
      * extract the domain part from the incoming email address
+     *  - including the @ symbol
      *
      * @param $email
      *
