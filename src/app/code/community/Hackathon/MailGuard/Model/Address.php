6e91f36cad4a29aa5f8977d3e142606ad48ad1ac
Matthias Zeis <admin@matthias-zeis.com>
2014-05-10 13:51:15 +0200

    Created address database table.

---
+++ src/app/code/community/Hackathon/MailGuard/Model/Address.php
@@ -0,0 +1,11 @@
+<?php
+
+class Hackathon_MailGuard_Model_Address extends Mage_Core_Model_Abstract
+{
+
+    protected function _construct()
+    {
+        $this->_init('hackathon_mailguard/address');
+    }
+
+}
