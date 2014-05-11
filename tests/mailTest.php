9cc5f6fff19cdb614db2c4a16c1460c9a8b9b498
avoelkl <a.voelkl@limesoda.com>
2014-05-11 13:05:07 +0200

    Updates testfile sending testmails with sendTransactional

--- tests/mailTest.php
+++ tests/mailTest.php
@@ -7,11 +7,12 @@
  * @package    Hackathon_MailGuard
  * @author     Anna Völkl <a.voelkl@limesoda.com>
  */
- 
+
+die('uncomment this line and set testing e-mail addresses in this file');
+$emails = array('anna@vape.net');// 'andreas.penz@icc.at','andre@pixelperfect.at','dmanners87@gmail.com','admin@matthias-zeis.com');
+
 require_once 'app/Mage.php';
 Mage::app();
-
-$emails = array('a.voelkl@limesoda.com', 'andreas.penz@icc.at','andre@pixelperfect.at','dmanners87@gmail.com','admin@matthias-zeis.com');
 
 // core/email
 foreach($emails as $email) {
@@ -19,21 +20,28 @@ foreach($emails as $email) {
 	            ->setType('text')
 	            ->setToName($email)
 	            ->setToEmail($email)
+                ->setSubject("core email")
+                ->setFromName('MailGuard')
 	            ->send();
 }
-			
+
+echo "Sending Mage_Core_Model_Email Mail: done<br />";
+
 // core/email_template			
 /* @var $emailModel Mage_Core_Model_Email_Template */
 $emailModel = Mage::getModel('core/email_template');
 
 foreach ($emails as $email) {
-    $emailModel->sendTransactional(Mage::getStoreConfig(Mage_Sitemap_Model_Observer::XML_PATH_ERROR_TEMPLATE),
-        Mage::getStoreConfig('trans_email/ident_general/email'),
-        $email,
-        null,
-        null
-    );
+    $template = Mage::getStoreConfig('checkout/payment_failed/template', 0);
+    $emailModel->setDesignConfig(array('area'=>'frontend', 'store'=>0))
+        ->sendTransactional(
+            $template,
+            Mage::getStoreConfig('checkout/payment_failed/identity', 0),
+            "core email template",
+            $email,
+            null
+        );
 }
-
-echo "emails sent";
+echo "Sending Mage_Core_Model_Email_Template Mail: ";
+var_dump($emailModel->getSentSuccess());
 ?>
